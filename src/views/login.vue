8ce23f049d2bd1d36d0d044da4b661279bc602d8
张恒 <1479363919@qq.com>
2020-03-24 16:44:19 +0800

    绘制登录表单

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,13 +2,39 @@
   <div class="login-container">
     <el-card class="my-card">
       <img src="../assets/logo_index.png" alt>
+      <el-form>
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:240px;margin-right:8px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登 录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "my-login"
+  data() {
+    return {
+      loginForm: {
+        mobile: "",
+        code: ""
+      }
+    };
+  }
 };
 </script>
 
