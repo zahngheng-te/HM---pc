a3fe2ca32d046c0a21d263d972a60e87d1e2f598
张恒 <1479363919@qq.com>
2020-03-26 14:07:58 +0800

    页面基础布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import login from '../views/login.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 
 const routes = [
     { path: '/login', component: login },
+    { path: '/', component: Home }
 
 ]
 
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-container class="home-container">
+    <el-aside class="my-aside" width="200px">Aside</el-aside>
+    <el-container>
+      <el-header class="my-header">Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "my-name"
+};
+</script>
+
+<style scoped lang='less'>
+.home-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+.my-aside {
+  background-color: #002244;
+}
+
+.my-header {
+  border-bottom: 1px solid #ddd;
+}
+</style>
